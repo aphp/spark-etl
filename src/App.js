1657a6c2571fe9f541bbb87d1c77e00c509f3cdc
Jean-François YUEN <jean-francois.yuen@dataiku.com>
2020-03-26 16:26:30 +0100

    display: use materia-ui components for flexibility

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import DataGrid from './DataGrid.js';
+import SearchInput from './SearchInput.js';
+
 import './App.css';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
@@ -43,8 +46,82 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-  },
+  }
 }));
+
+function hasSearchText(col, attributeCols, filter) {
+  if (!filter || filter.length === 0) {
+    return true;
+  }
+
+  for (const colName of attributeCols) {
+    if (typeof col[colName.name] ==='string' && col[colName.name].search(filter) >= 0) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function applySearchFilter(tables, filter) {
+  const { rows, columns, attributeCols } = tables;
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+
+    row._hasColumnDisplay = false;
+    for (let j = 0; j < row.columns.length; j++) {
+      const col = row.columns[j];
+      col._display = hasSearchText(col, attributeCols, filter);
+      row._hasColumnDisplay = row._hasColumnDisplay || col._display;
+    }
+
+    if (row._hasColumnDisplay) {
+      row._display = true;
+    } else {
+      row._display = hasSearchText(row, columns, filter);
+    }
+  }
+}
+
+
+function getColumns(rows, skip) {
+  const columns = [];
+  const colNames = Object.keys(rows[0]);
+  
+  for (let i = 0; i < colNames.length; i++ ) {
+    const name = colNames[i];
+    if (name.startsWith('_') || (skip && skip.includes(name))) {
+      continue;
+    }
+
+    columns.push({name: name})
+  }
+
+  return columns;
+}
+
+function buildKeyRows(rows) {
+  const keyRows = [];
+  
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    row._key = 'row-' + i;
+    const keyColumns = [];
+  
+    for (let j = 0; j < row.columns.length; j++) {
+      const col = row.columns[j];
+      col._key = row._key + '-col-' + j;
+      keyColumns.push(col);
+    }
+  
+    row.columns = keyColumns;
+    keyRows.push(row);
+  }
+  
+  return keyRows;
+}
+
 
 class TabbedApp extends React.Component {
   constructor(props) {
@@ -52,7 +129,8 @@ class TabbedApp extends React.Component {
     this.state = {
       tables: null,
       error: null,
-      currentTabIndex: 0
+      currentTabIndex: 0,
+      searchText: ''
     };
   }
 
@@ -61,8 +139,14 @@ class TabbedApp extends React.Component {
       .then(res => res.json())
       .then(
         (result) => {
+          const columns = getColumns(result, ['columns']);
+          const attributeCols = getColumns(result[0].columns);
+          const rows = buildKeyRows(result);
+          const tables = { rows, columns, attributeCols };
+          applySearchFilter(tables, '');
+        
           this.setState({
-            tables: result
+            tables: tables
           });
         },
         (error) => {
@@ -73,7 +157,7 @@ class TabbedApp extends React.Component {
       );
   }
   render() {  
-    const { error, tables, currentTabIndex } = this.state;
+    const { error, tables, currentTabIndex, searchText } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!tables) {
@@ -83,20 +167,31 @@ class TabbedApp extends React.Component {
       const handleChange = (event, newTabIndex) => {
         this.setState({currentTabIndex: newTabIndex});
       };      
+      
+      const updateSearchText = (e) => {
+        const searchText = e.target.value;
+        this.setState({ searchText: searchText });
+        applySearchFilter(tables, searchText);
+      }
+
+      const classes = this.props.classes;
       return (
-        <div className={this.props.classes.root}>
+        <div className={classes.root}>
           <AppBar position="static">
+          <Toolbar>
             <Tabs value={currentTabIndex} onChange={handleChange} aria-label="Tabs">
               <Tab label="Tables" {...a11yProps(0)} />
               <Tab label="Diagram" {...a11yProps(1)} />
             </Tabs>
+            <SearchInput updateSearchText={updateSearchText} searchText={searchText}></SearchInput>
+          </Toolbar>
           </AppBar>
           <TabPanel value={currentTabIndex} index={0}>
-            <DataGrid rows={tables} title='Table'/>
+            <DataGrid tables={tables} filter={searchText}/>
           </TabPanel>
           <TabPanel value={currentTabIndex} index={1}>
             Diagram
-          </TabPanel>
+          </TabPanel>      
         </div>
       );
     }
--- src/DataGrid.js
+++ src/DataGrid.js
@@ -1,60 +1,102 @@
 import React from "react";
-import MaterialTable from "material-table";
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function getColumns(rows, skip) {
-  const columns = [];
-  const colNames = Object.keys(rows[0]);
-  
-  for (let i = 0; i < colNames.length; i++ ) {
-    const name = colNames[i];
-    if (skip && skip.includes(name)) {
-      continue;
-    }
+const StyledHeader = withStyles(theme => ({
+  head: {
+    backgroundColor: '#1976d2',
+    color: theme.palette.common.white,
+    fontSize: 18,
+  },
+}))(TableCell);
 
-    columns.push({'field': name, 'title': name})
+
+const useStyles = makeStyles({
+  tableHead: {
+    backgroundColor: '#eeeeee',
+    fontSize: 16,
+  },
+});
+
+function DataRow({row, useName, cells, className}) {
+  if (!row._display) {
+    return null;
   }
 
-  return columns;
+  return (
+    <TableRow className={className} hover={true}>
+    {cells.map(col => (
+      <TableCell key={'table-cell-' + row._key + '-' + col.name}>
+        <React.Fragment>{useName ? row[col.name] : col.name}</React.Fragment>
+      </TableCell>
+    ))}
+  </TableRow>
+  );
 }
 
+function AttributeTable({row, columns, attributeCols}) {
+  if (!row._hasColumnDisplay) {
+    return null;
+  }
 
-function DataGrid({ rows, title }) { 
-  const columns = getColumns(rows, ['columns', 'tableData']);
-  const attributeCols = getColumns(rows[0].columns);
-  
   return (
-    <div style={{ maxWidth: "100%" }}>
-      <MaterialTable
-        options={{
-          paging: false,
-          rowStyle: x => {
-            if (x.tableData.id % 2) {
-              return { backgroundColor: "#efefef" }
-            }
-          },
-        }} 
-        detailPanel={[
-          {
-            tooltip: 'Attributs',
-            render: rowData => {
-              return (
-                <MaterialTable
-                  options={{
-                    paging: false
-                  }} 
-                  columns={attributeCols}
-                  data={rowData.columns}
-                  title={rowData.name + ': attributs'}
-                />
-              )
-            },
-          }
-        ]}             
-        columns={columns}
-        data={rows}
-        title={title}   
-      />
-    </div>
+    <TableRow hover={true}>
+      <TableCell colSpan={columns.length}>
+        <Table aria-label="table">
+          <TableHead>
+            <DataRow key={row._key + '-attributes-head'} row={row} useName={false} cells={attributeCols}></DataRow>
+          </TableHead>
+          <TableBody>
+            {row.columns.map(tableColumn => (
+              <DataRow row={tableColumn} useName={true} cells={attributeCols} key={row._key + '-attributes-' + tableColumn.name}></DataRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableCell>                  
+    </TableRow>); 
+}
+
+function TableData({row, columns, attributeCols}) {
+  const classes = useStyles();
+
+  if (!row._display) {
+    return null;
+  }
+
+  return (
+    <React.Fragment>
+      <DataRow className={classes.tableHead}  key={row._key + '-content'} row={row} useName={true} cells={columns}></DataRow>      
+      <AttributeTable row={row} columns={columns} attributeCols={attributeCols}></AttributeTable>
+    </React.Fragment>       
+  )
+}
+
+function DataGrid({ tables }) { 
+  const { rows, columns, attributeCols } = tables;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="table">
+        <TableHead>
+          <TableRow>
+            {columns.map(col => (
+              <StyledHeader key={'col-' + col.name}>{col.name}</StyledHeader>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableData key={'tabledata-' + row._key} row={row} columns={columns} attributeCols={attributeCols}></TableData>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 

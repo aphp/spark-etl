4bfab667ed4db8d69562178dcaf060052a4ac183
tlama <tristan.lama@gmail.com>
2020-04-06 12:41:21 +0200

    Add spark-postgres dependency to meta module

--- meta/src/main/scala/meta/MetaLoader.scala
+++ meta/src/main/scala/meta/MetaLoader.scala
@@ -27,9 +27,13 @@ class MetaLoader(host: String, database: String, schema: String, user: String) e
     //df
   }
 
+  /**
+   * Writing Dataframe into postgres table
+   * SCD1 writing type means "add new or update"
+   */
   def writeScd1(df: DataFrame, table: String, joinKey: String, filter: String, deleteSet: String): Unit = {
     logger.warn("loading scd1 with %d rows".format(df.count))
-    df.write.format("postgres")
+    df.write.format("io.frama.parisni.spark.postgres")
       .option("type", "scd1")
       .option("partitions", 4)
       .option("host", host)
@@ -48,33 +52,35 @@ class MetaLoader(host: String, database: String, schema: String, user: String) e
   }
 
   def loadSchema(df: DataFrame, dbName: String): Unit = {
-    val opt = Map("joinColumns" -> "lib_database", "pgJoinSql" ->
-      "select ids_database, lib_database from meta_database")
+    val opt = Map("joinColumns" -> "lib_database"
+      , "pgJoinSql" -> "select ids_database, lib_database from meta_database")
     val result = getLookup(df, opt)
     writeScd1(result, "meta_schema", "lib_database,lib_schema", s"lib_database = '$dbName'", "is_active = false")
   }
 
   def loadTable(df: DataFrame, dbName: String): Unit = {
-    val opt = Map("joinColumns" -> "lib_database,lib_schema", "pgJoinSql" ->
-      "select ids_schema, d.lib_database, s.lib_schema from meta_schema s join meta_database d using (ids_database)")
+    val opt = Map("joinColumns" -> "lib_database,lib_schema"
+      , "pgJoinSql" -> "select ids_schema, d.lib_database, s.lib_schema from meta_schema s join meta_database d using (ids_database)")
     val result = getLookup(df, opt)
     writeScd1(result, "meta_table", "lib_database,lib_schema,lib_table", s"lib_database = '$dbName'", "is_active = false")
   }
 
   def loadColumn(df: DataFrame, dbName: String): Unit = {
-    val opt = Map("joinColumns" -> "lib_database,lib_schema,lib_table", "pgJoinSql" ->
-      "select ids_table, d.lib_database, s.lib_schema, t.lib_table from meta_table t join meta_schema s using (ids_schema) join meta_database d using (ids_database)")
+    val opt = Map(
+      "joinColumns" -> "lib_database,lib_schema,lib_table"
+      , "pgJoinSql" -> "select ids_table, d.lib_database, s.lib_schema, t.lib_table from meta_table t join meta_schema s using (ids_schema) join meta_database d using (ids_database)"
+    )
     val result = getLookup(df, opt)
     writeScd1(result, "meta_column", "lib_database,lib_schema,lib_table,lib_column", s"lib_database = '$dbName'", "is_active = false")
   }
 
   def loadReference(df: DataFrame, dbName: String): Unit = {
-    val optSource = Map("joinColumns" -> "lib_database_source,lib_schema_source,lib_table_source,lib_column_source", "pgJoinSql" ->
-      "select ids_column as ids_source, d.lib_database as lib_database_source, s.lib_schema as lib_schema_source, t.lib_table as lib_table_source, c.lib_column as lib_column_source from meta_column c join meta_table t using (ids_table) join meta_schema s using (ids_schema) join meta_database d using (ids_database)")
+    val optSource = Map("joinColumns" -> "lib_database_source,lib_schema_source,lib_table_source,lib_column_source"
+      , "pgJoinSql" -> "select ids_column as ids_source, d.lib_database as lib_database_source, s.lib_schema as lib_schema_source, t.lib_table as lib_table_source, c.lib_column as lib_column_source from meta_column c join meta_table t using (ids_table) join meta_schema s using (ids_schema) join meta_database d using (ids_database)")
     val resultSource = getLookup(df, optSource)
 
-    val optTarget = Map("joinColumns" -> "lib_database_target,lib_schema_target,lib_table_target,lib_column_target", "pgJoinSql" ->
-      "select ids_column as ids_target, d.lib_database as lib_database_target, s.lib_schema as lib_schema_target, t.lib_table as lib_table_target, c.lib_column as lib_column_target from meta_column c join meta_table t using (ids_table) join meta_schema s using (ids_schema) join meta_database d using (ids_database)")
+    val optTarget = Map("joinColumns" -> "lib_database_target,lib_schema_target,lib_table_target,lib_column_target"
+      , "pgJoinSql" -> "select ids_column as ids_target, d.lib_database as lib_database_target, s.lib_schema as lib_schema_target, t.lib_table as lib_table_target, c.lib_column as lib_column_target from meta_column c join meta_table t using (ids_table) join meta_schema s using (ids_schema) join meta_database d using (ids_database)")
     val resultTarget = getLookup(resultSource, optTarget)
 
     writeScd1(resultTarget, "meta_reference", "lib_database_source,lib_schema_source,lib_table_source,lib_column_source,lib_database_target,lib_schema_target,lib_table_target,lib_column_target,lib_reference"

618c48871d41a7e43065ca4f48ab8dd472479abc
tlama <tristan.lama@gmail.com>
2020-04-06 18:20:37 +0200

    Add ddlTest

---
+++ meta/src/test/scala/meta/ddlTest.scala
@@ -0,0 +1,30 @@
+package meta
+
+import com.opentable.db.postgres.junit.{EmbeddedPostgresRules, SingleInstancePostgresRule}
+import org.junit.{Rule}
+import io.frama.parisni.spark.postgres.PGTool
+
+import scala.annotation.meta.getter
+
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionTestWrapper {
+
+  // looks like crazy but compatibility issue with junit rule (public)
+  @(Rule@getter)
+  var pg: SingleInstancePostgresRule = EmbeddedPostgresRules.singleInstance()
+
+  def getPgUrl = pg.getEmbeddedPostgres.getJdbcUrl("postgres", "postgres") + "&currentSchema=public"
+
+  def getPgTool() = PGTool(spark, getPgUrl, "/tmp")
+
+  lazy val spark: SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("spark session")
+      .config("spark.sql.shuffle.partitions", "1")
+      .getOrCreate()
+  }
+
+}

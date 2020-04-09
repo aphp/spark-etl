1652654ebc1adc943424750260f120fb6a3237c0
Jean-François YUEN <jean-francois.yuen@dataiku.com>
2020-04-09 20:53:57 +0200

    server: better error handling + remove old endpoints

--- server.js
+++ server.js
@@ -22,102 +22,6 @@ pool.on('connect', (client) => {
   client.query(`SET search_path TO "meta"`);
 });
 
-function splitComment(comment) {
-  const res = {comment_omop: '', comment_aphp: ''};
-  if (!comment) {
-    return res;
-  }
-  const splitComment = comment.split('- APHP Annotation :');
-  res.comment_omop = splitComment[0].replace('OMOP Comment : ', '');
-  if (splitComment.length == 2) {
-    res.comment_aphp = splitComment[1];
-  }
-  return res;
-}
-
-function nameSorter(a, b) {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
-  return 0;
-}
-
-const tablesData = [];
-const links = [];
-async function loadDbSchema() {
-
-  const db = await pgStructure({ database, user, password, host, port }, {  });
-  const tablesRef = {}
-
-  let id = 0;
-  for (let i = 0; i < db.tables.length; i++) {
-    const t = db.tables[i];
-
-    const rowKey = 'row-' + i;
-    
-    const columns = [];
-    for (let j = 0; j < t.columns.length; j++) {
-      const colKey = rowKey + '-col-' + j;
-      const c = t.columns[j];
-      columns.push({
-        id: id.toString(),
-        _key: colKey,
-        name: c.name,
-        type: c.type.shortName,
-        ...splitComment(c.comment)
-      });
-      columns.sort(nameSorter);
-      id += 1;
-    }
-    
-    const table = {
-      id: id.toString(),
-      _key: rowKey,
-      name: t.name,
-      ...splitComment(t.comment),
-      columns
-    }
-    tablesData.push(table);
-    tablesRef[t.name] = table.id;
-    id += 1;
-  }
-  tablesData.sort(nameSorter);
-
-  function addTarget(l, relation) {
-    const sourceTableId = tablesRef[relation.sourceTable.name];
-    const targetTableId = tablesRef[relation.targetTable.name];
-    // Do not add a link if it already exists in the other way
-    if (l.hasOwnProperty(targetTableId) && l[targetTableId].has(sourceTableId)) {
-      return;
-    }
-
-    if (!l.hasOwnProperty(sourceTableId)) {
-      l[sourceTableId] = new Set();
-    }
-    l[sourceTableId].add(targetTableId);
-  }
-
-  const linkSet = {}
-  for (const table of db.tables) {
-    for (relation of table.o2mRelations) {
-      addTarget(linkSet, relation);
-    }
-    // This is already taken into account with o2m and m2o relationships
-    // for (relation of table.m2mRelations) {
-    //   addTarget(linkSet, relation);
-    // }
-    for (relation of table.m2oRelations) {
-      addTarget(linkSet, relation);
-    }
-  }
-  for (const source in linkSet) {
-    for (const target of linkSet[source]) {
-      links.push({source, target});
-    }
-  }
-}
-loadDbSchema();
-
-
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -126,15 +30,16 @@ app.get('/schema', function (req, res) {
 });
 
 
-// New endpoints
 app.get('/databases', (req, res) => {
   pool.query('SELECT ids_database AS id, lib_database AS name FROM meta_database WHERE COALESCE(is_active_m, is_active) ORDER BY lib_database ASC', (error, results) => {
     if (error) {
-      throw error
+      console.log('An error occured when fetching schemas:', error);
+    } else {
+      res.json(results.rows);
     }
-    res.json(results.rows);
   });
 });
+
 
 app.get('/schemas', function (req, res) {
   pool.query({
@@ -147,9 +52,10 @@ app.get('/schemas', function (req, res) {
     values: [req.query.ids_database]
   }, (error, results) => {
     if (error) {
-      throw error
+      console.log('An error occured when fetching schemas:', error);
+    } else {
+      res.json(results.rows);
     }
-    res.json(results.rows);
   });
 });
 
@@ -167,7 +73,7 @@ function createLinks(references, tableNameIndex) {
     if (!linkSet.hasOwnProperty(sourceTableId)) {
       linkSet[sourceTableId] = new Set();
     }
-    linkSet[sourceTableId].add(targetTableId);        
+    linkSet[sourceTableId].add(targetTableId);
   }
 
   const links = [];
@@ -211,7 +117,7 @@ app.get('/tables', (req, res) => {
             AND COALESCE(mt.is_active_m, mt.is_active) \
             ORDER BY mc.ids_table, name ASC',
       values: [req.query.ids_schema]
-    }),    
+    }),
     pool.query({
       text: 'SELECT ids_reference AS id, \
             COALESCE(lib_reference_m, lib_reference) AS name, \
@@ -252,14 +158,14 @@ app.get('/tables', (req, res) => {
       });
     })
     .catch(err => {
-      throw err;
+      console.log('An error occured when fetching tables:', err);
     });
 });
- 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const serverPort = process.env.PORT || 8080;
 console.log('server running on port: ' + serverPort);
-app.listen(serverPort); 
+app.listen(serverPort);

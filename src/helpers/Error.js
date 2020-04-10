975f2b6bcdd096fcf2a8f500c71300b4f8446796
Jean-François YUEN <jean-francois.yuen@dataiku.com>
2020-04-10 23:17:03 +0200

    display: improve error style

--- src/helpers/Error.js
+++ src/helpers/Error.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
     error: {
@@ -8,14 +10,21 @@ const useStyles = makeStyles(theme => ({
   }));
 
 function Error(props) {
-    const { error } = props;
-    const classes = useStyles();
+  const { error } = props;
+  const classes = useStyles();
 
-    if (!error) {
-        return null;
-    }
+  if (!error) {
+      return null;
+  }
 
-    return <h3 className={classes.error}>Error: {error.message}</h3>;
+  return (
+    <Box display="flex" style={{ width: '100%' }}>
+      <Box m="auto">
+        <Typography component="div" bgcolor="background.paper">
+          <h3 className={classes.error}>Error: {error.message}</h3>
+        </Typography>
+      </Box>
+    </Box>);
 }
 
 export default Error;
